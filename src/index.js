d81c47b495b8f3a8913f407e133ea4800be50fd3
yurkevich-serj <yurkevichserj@gmail.com>
2018-03-15 18:10:53 +0300

    done first part

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
+const createEnumerableProperty = (propertyName) => { return propertyName };
+const createNotEnumerableProperty = (propertyName) => { return Symbol.for(propertyName) };
 const createProtoMagicObject = () => {};
 const incrementor = () => {};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (value) => { return new Promise((resolve) => {
+         setTimeout(() => {
+           resolve(value);}, 1000);
+     });};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => { return Object('yeap') };
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (array) => { return array.sort((left,right) => left - right) };
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
